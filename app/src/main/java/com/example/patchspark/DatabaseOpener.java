75e4904de9265774bd2c0e88ebe4de8b79374a43
Peter-Mizanski <pmizanski@gmail.com>
2024-01-21 18:36:47 -0500

    Class DatabaseOpener

---
+++ app/src/main/java/com/example/patchspark/DatabaseOpener.java
@@ -0,0 +1,40 @@
+package com.example.patchspark;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseOpener extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "PatchSparkDB";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "MODULE_PROMPTS";
+    private static final String COLUMN_MAKE = "MAKE";
+    private static final String COLUMN_MODULE = "MODULE";
+    private static final String COLUMN_TITLE = "TITLE";
+    private static final String COLUMN_PROMPT = "PROMPT";
+    private static final String COLUMN_INCLUDE = "INCLUDE";
+
+
+
+    public DatabaseOpener(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    public void onCreate(SQLiteDatabase database) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + " (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_MAKE + " TEXT, " +
+                COLUMN_MODULE + " TEXT, " +
+                COLUMN_TITLE + " TEXT, " +
+                COLUMN_PROMPT + " TEXT, " +
+                COLUMN_INCLUDE + " INTEGER);";
+        database.execSQL(createTableQuery);
+    }
+
+
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        database.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(database);
+    }
+}
